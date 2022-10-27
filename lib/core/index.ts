b962e3e239d9a9b90f7e06a1a76ade6b308f0423
Kevin Jobs <me@kevinjobs.com>
2022-10-27 18:37:11 +0800

    feat: add multi config

--- lib/core/index.ts
+++ lib/core/index.ts
@@ -1,3 +1,18 @@
-export default async function MintServer() {
-  console.log("this is Mint Server");
+import Koa from "koa";
+import path from "path";
+
+type Params = {
+  appPath: string;
+}
+
+export default async function MintServer(params: Params) {
+  const app = new Koa;
+  const { appPath } = params;
+  app.appPath = appPath;
+
+  const env = process.env.NODE_ENV;
+  const ext = app.extName = env === "development" ? ".ts" : ".js";
+  const base = await import(path.join(appPath, `config/config.base&{ext}`));
+  const cur = await import(path.join(appPath, `config/config.${env}${ext}`));
+  app.config = base.default(app) + cur.default(app);
 }
